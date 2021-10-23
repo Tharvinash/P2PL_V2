07b35b8df973127d009cc1bf9108ff2453734153
THARVINASH VISVANATHAN <tharvinjr24@gmail.com>
2021-10-23 15:04:39 +0800

    ui mm updated

--- components/main/component/mmCommentCard.js
+++ components/main/component/mmCommentCard.js
@@ -1,66 +1,75 @@
-import React from "react";
+import React from 'react';
 import {
   View,
   Text,
   StyleSheet,
   TouchableOpacity,
   TextInput,
-} from "react-native";
-import MentionsTextInput from "react-native-mentions";
-import { Icon } from "react-native-elements";
+} from 'react-native';
+import MentionsTextInput from 'react-native-mentions';
+import { Icon } from 'react-native-elements';
 
 const mmCommentCard = (props) => {
   return (
-    <View style={{ justifyContent: "center" }}>
-      <View style={styles.searchSection}>
-        <MentionsTextInput
-          suggestionsPanelStyle={{
-            backgroundColor: "rgba(100,100,100,0.1)",
-          }}
-          loadingComponent={props.loadingComponent}
-          textInputMinHeight={30}
-          textInputMaxHeight={80}
-          trigger={"@"}
-          triggerLocation={"new-word-only"} // 'new-word-only', 'anywhere'
-          value={props.caption}
-          onChangeText={props.setCaption}
-          triggerCallback={props.callback}
-          renderSuggestionsRow={props.renderSuggestionsRow}
-          suggestionsData={props.datas}
-          keyExtractor={props.keyExtractor}
-          suggestionRowHeight={45}
-          horizontal={false}
-          MaxVisibleRowCount={3}
-        />
-        <Icon
-          style={styles.searchIcon}
-          name="attach-outline"
-          type="ionicon"
-          size={30}
-          color="#000"
-          // onPress={() => {
-          //   pickDocument();
-          // }}
-          onPress={props.pickDocument}
-        />
-        <Icon
-          style={styles.searchIcon}
-          name="image-outline"
-          type="ionicon"
-          size={30}
-          color="#000"
-          // onPress={() => {
-          //   pickImage();
-          // }}
-          onPress={props.pickImage}
-        />
+    <View style={{ justifyContent: 'center' }}>
+      {/* style={styles.searchSection} */}
+      <View style={{ flexDirection: 'row' }}>
+        <View style={{ flex: 1, justifyContent: 'flex-start' }}>
+          <View style={{ width: '100%' }}>
+            <MentionsTextInput
+              textInputStyle={styles.searchSection}
+              suggestionsPanelStyle={{
+                backgroundColor: '#fff',
+              }}
+              loadingComponent={props.loadingComponent}
+              textInputMinHeight={30}
+              textInputMaxHeight={80}
+              trigger={'@'}
+              triggerLocation={'new-word-only'} // 'new-word-only', 'anywhere'
+              value={props.caption}
+              onChangeText={props.setCaption}
+              triggerCallback={props.callback}
+              renderSuggestionsRow={props.renderSuggestionsRow}
+              suggestionsData={props.datas}
+              keyExtractor={props.keyExtractor}
+              suggestionRowHeight={45}
+              horizontal={false}
+              MaxVisibleRowCount={3}
+            />
+          </View>
+        </View>
+
+        <View style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
+          <Icon
+            style={{ marginTop: 5 }}
+            name='attach-outline'
+            type='ionicon'
+            size={30}
+            color='#fff'
+            // onPress={() => {
+            //   pickDocument();
+            // }}
+            onPress={props.pickDocument}
+          />
+          <Icon
+            style={styles.searchIcon}
+            name='image-outline'
+            type='ionicon'
+            size={30}
+            color='#fff'
+            // onPress={() => {
+            //   pickImage();
+            // }}
+            onPress={props.pickImage}
+          />
+        </View>
       </View>
 
       <View
         style={{
-          flexDirection: "row",
-          justifyContent: "center",
-          alignContent: "space-between",
+          flexDirection: 'row',
+          justifyContent: 'center',
+          alignContent: 'space-between',
         }}
       >
         <View
@@ -95,41 +104,36 @@ const mmCommentCard = (props) => {
 
 const styles = StyleSheet.create({
   searchSection: {
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#fff",
-    borderColor: "#E3562A",
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    borderColor: '#E3562A',
     borderWidth: 1,
     borderRadius: 12,
     padding: 8,
-  },
-  input: {
-    flex: 1,
     paddingTop: 10,
     paddingRight: 10,
     paddingBottom: 10,
-    paddingLeft: 0,
-    backgroundColor: "#fff",
-    color: "#424242",
+    paddingLeft: 10,
   },
 
   blogout: {
     width: 140,
     height: 40,
-    backgroundColor: "#E3562A",
-    borderColor: "#E3562A",
+    backgroundColor: '#E3562A',
+    borderColor: '#E3562A',
     borderRadius: 16,
     marginTop: 20,
   },
 
   Ltext: {
-    color: "#fff",
-    textAlign: "center",
-    fontFamily: "Poppins",
-    fontWeight: "700",
+    color: '#fff',
+    textAlign: 'center',
+    fontFamily: 'Poppins',
+    fontWeight: '700',
     fontSize: 15,
-    justifyContent: "space-between",
+    justifyContent: 'space-between',
     paddingTop: 8,
   },
 });
